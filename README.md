da3416922d77d50c85d731003d6364f8f9646624
Ben Rady <ben.rady@aquatic.com>
2022-05-05 19:04:27 -0500

    adding initial speed as an optional argument

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Example
+
+
+```
+$ python3 ./stimpmeter.py 2 10
+On a stimp 10 green with a 2.0% slope at 72 inches per second (4.090909090909091 mph):
+Went up 7.831865 feet (-2.168135) in 2.612000 seconds
+Up Scaling factor: 1.280518
+Went down 13.938076 feet (+3.938076) in 4.648000 seconds
+Down Scaling factor: 0.719529
+```
--- stimpmeter.py
+++ stimpmeter.py
@@ -11,7 +11,8 @@ COF = {
         12: 0.0655,
         13: 0.0605}
 
-def calculate(angle, stimp):
+def calculate(percent_grade, stimp, v=72):
+    degrees = percent_grade / 2.22
     friction = COF[stimp]
     ticks_per_second = 1000
     g = 32 * 12 # Gravity in inches per second per second
@@ -21,14 +22,13 @@ def calculate(angle, stimp):
     # Force of friction
     drag = -(5/7) * friction * g * period
 
-    # Ball is moving 72 inches per second at the bottom of a stimpmeter ramp
-    v = 72
     ###
 
     # Force of gravity, deflected by the slope
-    drop = -g * sin(radians(angle)) * period
+    drop = -g * sin(radians(degrees)) * period
 
-    assert drag + drop < 0, "Ball will never stop!"
+    if drag + drop >= 0:
+        return None, None
 
     distance = 0
     seconds = 0
@@ -40,13 +40,28 @@ def calculate(angle, stimp):
 
     return (distance / 12), seconds
 
-slope = float(sys.argv[1]) # Slope in percent grade
-stimp = int(sys.argv[2]) # Speed of the green as measured by a stimpmeter
-degrees = slope / 2.22
-print(f"On a stimp {stimp} green with a {slope}% slope:")
+if __name__ == "__main__":
+    if (len(sys.argv) < 3):
+        print("Usage: python3 ./stimpmeter.py <slope in % grade> <speed in stimp> [initial speed in in/sec]")
+        sys.exit(0)
+    slope = float(sys.argv[1]) # Slope in percent grade
+    stimp = int(sys.argv[2]) # Speed of the green as measured by a stimpmeter
 
-distance, seconds = calculate(degrees, stimp)
-print("Went up %f feet (%f) in %f seconds" % (distance, distance-stimp, seconds))
+# Ball is moving 72 inches per second at the bottom of a stimpmeter ramp, so that's the default velocity
+    v = 72
+    if len(sys.argv) == 4:
+        v = float(sys.argv[3])
 
-distance, seconds = calculate(-degrees, stimp)
-print("Went down %f feet (+%f) in %f seconds" % (distance, distance-stimp, seconds))
+    print(f"On a stimp {stimp} green with a {slope}% slope at {v} inches per second ({v/17.6} mph):")
+
+    flat_distance, flat_seconds = calculate(0, stimp, v)
+
+    up_distance, up_seconds = calculate(slope, stimp, v)
+
+    print("Went up %f feet (%f) in %f seconds" % (up_distance, up_distance - stimp, up_seconds))
+    print("Up Scaling factor: %f" % (flat_distance / up_distance))
+
+    down_distance, down_seconds = calculate(-slope, stimp, v)
+    assert down_distance is not None, "Ball will never stop going down!"
+    print("Went down %f feet (+%f) in %f seconds" % (down_distance, down_distance - stimp, down_seconds))
+    print("Down Scaling factor: %f" % (flat_distance / down_distance))
